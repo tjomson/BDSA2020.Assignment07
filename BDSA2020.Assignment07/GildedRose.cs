baf2406e210568abe2029846ce2c1bbf925f9d38
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-11-10 14:47:16 +0100

    More shorter mmmm

--- BDSA2020.Assignment07/GildedRose.cs
+++ BDSA2020.Assignment07/GildedRose.cs
@@ -23,7 +23,6 @@ namespace GildedRose
                     case "Aged Brie":
                         if (item.SellIn > 0) item.Quality++;
                         else item.Quality += 2;
-                        item.SellIn--;
                         break;
                     case "Sulfuras, Hand of Ragnaros":
                         isLegendary = true;
@@ -33,15 +32,13 @@ namespace GildedRose
                         else if (item.SellIn < 6) item.Quality += 3;
                         else if (item.SellIn < 11) item.Quality += +2;
                         else item.Quality++;
-                        item.SellIn--;
                         break;
                     default:
                         if (item.Quality > 0) item.Quality -= conjuredMultiplier;
-                        item.SellIn--;
-                        if (item.SellIn < 0) item.Quality -= conjuredMultiplier;
+                        if (item.SellIn < 1) item.Quality -= conjuredMultiplier;
                         break;
                 }
-                
+                if (!isLegendary) item.SellIn--;
                 if (item.Quality > 50 && !isLegendary) item.Quality = 50;
                 else if (item.Quality < 0) item.Quality = 0;
             }
